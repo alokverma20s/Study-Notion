ee4c28d019be8da2a66df675f0f236a81e658dee
Alok Verma <alokverma73762@gmail.com>
2023-12-10 02:08:32 +0530

    Stored frontend site in env file

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://studynotion-alok.netlify.app",
+		origin: process.env.FRONTEND_URL,
 		credentials:true,
 	})
 )
