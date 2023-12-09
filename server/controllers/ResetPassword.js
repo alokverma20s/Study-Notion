67e2ac75cc87f5a29a1d45e0c1643e001e39a1d5
Alok Verma <alokverma73762@gmail.com>
2023-12-10 01:54:31 +0530

    fixing some error

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -25,7 +25,7 @@ export const resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `http://localhost:3000/update-password/${token}`;
+		const url = `https://studynotion-alok.netlify.app/update-password/${token}`;
 
 		await mailSender(
 			email,
--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000" || "https://studynotion-alok.netlify.app/",
+		origin:"https://studynotion-alok.netlify.app",
 		credentials:true,
 	})
 )
